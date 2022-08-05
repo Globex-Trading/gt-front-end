e4dc397aa7f1ccb73d9712b6e3ed8e155300910b
dn00z <hgdnimantha@gmail.com>
2022-08-05 11:07:16 +0530

    bug fixed

--- src/components/common/chart.jsx
+++ src/components/common/chart.jsx
@@ -1,5 +1,5 @@
 import { createChart, CrosshairMode } from 'lightweight-charts';
-import React, {useRef} from 'react';
+import React, {useEffect, useRef} from 'react';
 
 const addSeriesFunction = {
 	candleStick: 'addCandleStickSeries',
@@ -10,54 +10,52 @@ const Chart = (props) => {
 
 	const chartContainerRef = useRef();
 
-	const chart = createChart(chartContainerRef.current,
-		{
-			width: 1000,
-			height: 600,
-			layout: {
-				backgroundColor: '#000000',
-				textColor: 'rgba(255, 255, 255, 0.9)',
-			},
-			grid: {
-				vertLines: {
-					color: 'rgba(197, 203, 206, 0.5)',
+	useEffect(() => {
+		const chart = createChart(chartContainerRef.current,
+			{
+				width: 1000,
+				height: 600,
+				layout: {
+					backgroundColor: '#000000',
+					textColor: 'rgba(255, 255, 255, 0.9)',
 				},
-				horzLines: {
-					color: 'rgba(197, 203, 206, 0.5)',
+				grid: {
+					vertLines: {
+						color: 'rgba(197, 203, 206, 0.5)',
+					},
+					horzLines: {
+						color: 'rgba(197, 203, 206, 0.5)',
+					},
 				},
-			},
-			crosshair: {
-				mode: CrosshairMode.Normal,
-			},
-			rightPriceScale: {
-				borderColor: 'rgba(197, 203, 206, 0.8)',
-			},
-			timeScale: {
-				borderColor: 'rgba(197, 203, 206, 0.8)',
-			},
-		}
-	);
-	chart.timeScale().fitContent();
+				crosshair: {
+					mode: CrosshairMode.Normal,
+				},
+				rightPriceScale: {
+					borderColor: 'rgba(197, 203, 206, 0.8)',
+				},
+				timeScale: {
+					borderColor: 'rgba(197, 203, 206, 0.8)',
+				},
+			}
+		);
+		chart.timeScale().fitContent();
 
-	const func = addSeriesFunction[props.chartType];
+		const func = addSeriesFunction[props.chartType];
 
-	console.log(func);
-	console.log(chart,'-----');
-	const chartInstance = chart['addCandlestickSeries'](
-		{
-			upColor: 'rgba(255, 144, 0, 1)',
-			downColor: '#000',
-			borderDownColor: 'rgba(255, 144, 0, 1)',
-			borderUpColor: 'rgba(255, 144, 0, 1)',
-			wickDownColor: 'rgba(255, 144, 0, 1)',
-			wickUpColor: 'rgba(255, 144, 0, 1)',
-		}
-	);
-	chartInstance.setData(props.data);
-
-
-	// useEffect(() => {
-	// }, [props.data, props.chartType]);
+		console.log(func);
+		console.log(chart,'-----');
+		const chartInstance = chart['addCandlestickSeries'](
+			{
+				upColor: 'rgba(255, 144, 0, 1)',
+				downColor: '#000',
+				borderDownColor: 'rgba(255, 144, 0, 1)',
+				borderUpColor: 'rgba(255, 144, 0, 1)',
+				wickDownColor: 'rgba(255, 144, 0, 1)',
+				wickUpColor: 'rgba(255, 144, 0, 1)',
+			}
+		);
+		chartInstance.setData(props.data);
+	}, [props.data, props.chartType]);
 
 	return (
 		<div ref={chartContainerRef}/>

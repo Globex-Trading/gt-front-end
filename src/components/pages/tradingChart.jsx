f72232bbbeed50aaf23fbf66b99b365b52d316ff
dn00z <hgdnimantha@gmail.com>
2022-09-16 15:39:27 +0530

    connect with WebSocket connection

--- src/components/pages/tradingChart.jsx
+++ src/components/pages/tradingChart.jsx
@@ -1,5 +1,9 @@
 import React, {Fragment, useEffect, useState} from 'react';
 import Chart from '../common/chart';
+import SockJS from 'sockjs-client';
+import {over} from 'stompjs';
+
+let stompClient = null;
 
 const TradingChart = () => {
 	const [chartType, setChartType] = useState('line');
@@ -8,24 +12,71 @@ const TradingChart = () => {
 	const [data, setData] = useState([]);
 	const [tradeType, setTradeType] = useState('crypto');
 
+
 	useEffect(() => {
-		//TODO: get data from API
-		console.log('in trading view');
-	}, [chartType, interval, symbol, tradeType]);
+		console.log('useEffect 2 ', stompClient);
+
+		if(!stompClient) {
+			connectToServer();
+		}
+
+	}, [chartType, interval, symbol, tradeType, stompClient]);
+
+
+	const connectToServer = () => {
+		let sock = new SockJS('http://ec2-52-23-157-217.compute-1.amazonaws.com:8080/ws');
+		stompClient = over(sock);
+		stompClient.connect({}, onConnected, onError);
+		console.log('stompClient', stompClient);
+	};
+
+	const onConnected = (frame) => {
+		console.log('onConnected', frame);
+		console.log('onConnected', stompClient);
+		subscribeToTopic('/topic/binance_BTCUSDT_1m');
+	};
+
+	const onError = (error) => {
+		console.log(error,'00000000000000000');
+
+	};
+
+	const subscribeToTopic = (topic) => {
+		stompClient.subscribe(topic, (message) => {
+			const tradingData = JSON.parse(message.body);
+			if(!tradingData.klineClosed) {
+
+				console.log('------------', message);
+
+				const chartData = {
+					time: tradingData.openTime,
+					open: tradingData.openPrice,
+					high: tradingData.highPrice,
+					low: tradingData.lowPrice,
+					close: tradingData.closePrice
+				};
+
+				console.log('data',data);
+				const newData = data;
+				newData.push(chartData);
+				console.log('newData',newData);
+				setData(newData);
+				// console.log('data', [...data, chartData]);
+			}
+		});
+	};
+
 	return (
 		<Fragment>
-			<Chart data={[
-				{ time: '2018-12-22', open: 75.16, high: 82.84, low: 36.16, close: 45.72 },
-				{ time: '2018-12-23', open: 45.12, high: 53.90, low: 45.12, close: 48.09 },
-				{ time: '2018-12-24', open: 60.71, high: 60.71, low: 53.39, close: 59.29 },
-				{ time: '2018-12-25', open: 68.26, high: 68.26, low: 59.04, close: 60.50 },
-				{ time: '2018-12-26', open: 67.71, high: 105.85, low: 66.67, close: 91.04 },
-				{ time: '2018-12-27', open: 91.04, high: 121.40, low: 82.70, close: 111.40 },
-				{ time: '2018-12-28', open: 111.51, high: 142.83, low: 103.34, close: 131.25 },
-				{ time: '2018-12-29', open: 131.33, high: 151.17, low: 77.68, close: 96.43 },
-				{ time: '2018-12-30', open: 106.33, high: 110.20, low: 90.39, close: 98.10 },
-				{ time: '2018-12-31', open: 109.87, high: 114.69, low: 85.66, close: 111.26 },
-			]} chartType={'candleStick'}/>
+			<Chart data={data} chartType={'candleStick'}/>
+			<button className='mt-5 p-5' onClick={() => {
+				console.log(data);
+				setData([...data, 'test']);
+			}}
+			>
+				Connect to server
+			</button>
+			<span>{stompClient?.toString()}</span>
 		</Fragment>
 	);
 };

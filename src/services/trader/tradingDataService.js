7ca7bc8e43825798047f2bccbd0a7dccad379007
dn00z <hgdnimantha@gmail.com>
2022-09-25 11:26:34 +0530

    calling endpoints

---
+++ src/services/trader/tradingDataService.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+import { apiUrl } from '../config.json';
+
+//calling endpoint to get past trading data
+export async function getPastTradingData() {
+	// TODO: call endpoint to get past trading data
+}

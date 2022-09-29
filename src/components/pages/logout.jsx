487152ddbf5b99756daf8ce9cec3d4d089a4ee04
dn00z <hgdnimantha@gmail.com>
2022-09-29 11:58:21 +0530

    got user details

---
+++ src/components/pages/logout.jsx
@@ -0,0 +1,17 @@
+import React, {useEffect} from 'react';
+import {useNavigate} from 'react-router-dom';
+import {logout} from '../../services/authService';
+
+const Logout = () => {
+
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		logout();
+		navigate('/');
+	}, []);
+
+	return null;
+};
+
+export default Logout;

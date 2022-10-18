f4a7c46185cf0c8c8263057c75b47165c9742e16
dn00z <hgdnimantha@gmail.com>
2022-10-18 15:00:48 +0530

    added not found page

---
+++ src/components/pages/404.jsx
@@ -0,0 +1,19 @@
+import React, {Fragment} from 'react';
+
+const Page404 = () => {
+	return (
+		<Fragment>
+			<section
+				id="login"
+				className="section welcome-area login-styles overflow-hidden d-flex align-items-center"
+			>
+				<div className="container">
+					<h1 className="text-white">404</h1>
+					<p className="text-white">Page not found</p>
+				</div>
+			</section>
+		</Fragment>
+	);
+};
+
+export default Page404;

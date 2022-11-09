3389125eeff6686069ccf9b7fcb5b0ba4393cc19
sanduninduwara <sanduninduwara2@gmail.com>
2022-11-09 18:27:28 +0530

    add background

--- src/components/pages/profile.jsx
+++ src/components/pages/profile.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {getUser} from '../../services/authService';
 import PreLoader from '../common/loader';
+// import bi from 'assets/img/welcome/welcome1.png';
 
 const Profile = () => {
 	const [isLoading, setIsLoading] = useState(true);
@@ -21,6 +22,25 @@ const Profile = () => {
 		//TODO: set user details
 	};
 
+	const myStyle1={
+		backgroundImage: 'url(assets/img/back3.webp)',
+		backgroundSize: 'cover',
+		height: '100vh',
+		opacity: '0.9'
+	};
+
+	const myStyle2={
+		backgroundColor:'rgba(255, 255, 255, 0.65)',
+		// backgroundSize: 'cover',
+		// height: '100vh',
+		// opacity: '0.7'
+	};
+
+	const pp={
+		borderRadius:'100%',
+		height:'150px'
+	};
+
 	return (
 		<React.Fragment>
 			<PreLoader isLoading={isLoading}/>
@@ -28,48 +48,65 @@ const Profile = () => {
 				id="watchlist"
 				className="section bg-overlay overflow-hidden"
 			>
-				<div className='watchlist-container d-flex justify-content-center'>
-					<div className='container d-flex justify-content-center'>
-						<div className='my-5 p-5 rounded-lg w-75 shadow-lg '>
-							<div className='mb-4 text-black-100 font-italic'>
-								<h2 className='text-left'>Personal Details</h2>
+				<div className='watchlist-container d-flex justify-content-center' id='bg1' style={myStyle1} >
+					<div className='container d-flex justify-content-center' >
+						<div className='my-5 p-5 rounded-lg w-75 shadow-lg ' style={myStyle2}>
+							<div className='mb-4 text-black-100 '>
+								<h2 >Personal Details</h2>
 								<hr/>
-								<div className='row p-1 bg-grey' >
-									<div className='col-4 text-left'>
-										<div> First Name </div>
+
+								<div className='row'>
+									<div className='col-md-6'>
+										<img
+											// className="welcome-animation d-block"
+											src="assets/img/profile2.png"
+											alt=""
+											style={pp}
+										/>
+
 									</div>
-									<div className='col-8 text-left'>
-										<div>: Dasun </div>
+									<div className='col-md-6'>
+										<div className='row mt-4 p-1 bg-grey' style={myStyle2} >
+											<div className='col-4 text-left'>
+												<div> First Name </div>
+											</div>
+											<div className='col-8 text-left'>
+												<div>: Dasun </div>
+											</div>
+										</div>
+										<div className='row p-1'>
+											<div className='col-4 text-left'>
+												<div> Last Name </div>
+											</div>
+											<div className='col-8 text-left'>
+												<div>: Nimantha </div>
+											</div>
+										</div>
+										<div className='row p-1 bg-grey'>
+											<div className='col-4 text-left'>
+												<div> Email </div>
+											</div>
+											<div className='col-8 text-left'>
+												<div>: hgdnimantha@gmail.com </div>
+											</div>
+										</div>
+										
 									</div>
+
 								</div>
-								<div className='row p-1'>
-									<div className='col-4 text-left'>
-										<div> Last Name </div>
-									</div>
-									<div className='col-8 text-left'>
-										<div>: Nimantha </div>
-									</div>
-								</div>
-								<div className='row p-1 bg-grey'>
-									<div className='col-4 text-left'>
-										<div> Email </div>
-									</div>
-									<div className='col-8 text-left'>
-										<div>: hgdnimantha@gmail.com </div>
-									</div>
-								</div>
+								
 							</div>
-							<div className='row'>
+							<div className='row mt-5'>
 								<div className='col-md-6'>
-									<h3 className='text-left font-italic'>Watchlist</h3>
+									<h3 className='text-left '>Watchlist</h3>
 									<hr/>
-									<div className='h6 font-italic text-left pb-2'>You can add your favourite symbols into watchlist.</div>
+									<div className='h6 text-left pb-2'>You can add your favourite symbols into watchlist.</div>
 									<div className='font-weight-bold h6 bg-color-3 rounded p-2 cursor-1 w-50'>See More <i className='fa-external-link-square-alt'/> </div>
 								</div>
 								<div className='col-md-6'>
-									<h3 className='text-left font-italic'>Price Alerts</h3>
+									<h3 className='text-left '>Price Alerts</h3>
 									<hr/>
-									<div className='h6 font-italic text-left pb-2'>You can add price alerts for any symbol</div>
+									<div className='h6 text-left pb-2'>You can add price alerts for any symbol</div>
 									<div className='font-weight-bold h6 bg-color-3 rounded p-2 cursor-1 w-50'>See More <i className='fa-external-link-square-alt'/> </div>
 								</div>
 							</div>

9542490493ffa388cd8e8df287c2aa8789f8baf9
dn00z <hgdnimantha@gmail.com>
2022-11-13 17:53:26 +0530

    bug fixed

--- src/components/common/protectedRoute.jsx
+++ src/components/common/protectedRoute.jsx
@@ -2,16 +2,19 @@ import React, {useEffect, useState} from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import {getUser} from '../../services/authService';
 import toast from 'react-hot-toast';
+import {isTokenExpired} from '../../services/httpService';
 
-const ProtectedRoute = () => {
-	let user;
+const ProtectedRoute = (props) => {
 	const token = localStorage.getItem('user_token');
-	if (!token) user = null;
+
 	const payload = JSON.parse(atob(token.split('.')[1]));
-	console.log('payload', payload);
 
-
-	return user ? <Outlet /> : <Navigate to="/login" />;
+	if (payload?.id && !isTokenExpired() && props.userTypes.includes(payload?.type)) {
+		return <Outlet />;
+	}else {
+		toast.error('You are not authorized to view this page');
+		return <Navigate to="/" />;
+	}
 };
 
 export default ProtectedRoute;

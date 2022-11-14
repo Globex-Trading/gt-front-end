a52f82ff9b8c9d9d8bb60d3b2ec5eda789ab0b6e
dn00z <hgdnimantha@gmail.com>
2022-11-14 10:19:15 +0530

    added watchlist and alerts into profile

--- src/components/pages/profile.jsx
+++ src/components/pages/profile.jsx
@@ -1,20 +1,58 @@
 import React, {useEffect, useState} from 'react';
 import {getUser} from '../../services/authService';
 import PreLoader from '../common/loader';
+import Table from 'react-bootstrap/Table';
+import {getWatchList} from '../../services/profileService';
+import {getExistingAlertsByUserID} from '../../services/chartService';
+import toast from 'react-hot-toast';
 // import bi from 'assets/img/welcome/welcome1.png';
 
 const Profile = () => {
 	const [isLoading, setIsLoading] = useState(true);
 	const [user, setUser] = useState({});
+	const [watchList, setWatchList] = useState([]);
+	const [alerts, setAlerts] = useState([]);
 
 	useEffect(() => {
 		setIsLoading(true);
 		getUserDetails();
+		getWatchlist();
+		getExistingAlerts();
 		setTimeout(() => {
 			setIsLoading(false);
 		}, 2000);
 
 	}, []);
+
+	const getWatchlist = async () => {
+		const userId = localStorage.getItem('user_id');
+		try{
+			const watchlist = await getWatchList(userId);
+			if(watchlist?.data){
+				setWatchList(watchlist?.data);
+			}else {
+				setWatchList([]);
+			}
+		}catch (e) {
+			console.log(e);
+		}
+	};
+
+	const getExistingAlerts = async() => {
+		try {
+			const userId = localStorage.getItem('user_id');
+			const {data} = await getExistingAlertsByUserID(userId);
+			if(data.status === 'SUCCESS'){
+				setAlerts(data.data);
+			}else {
+				toast.error(data.data);
+			}
+		}catch (e) {
+			console.log(e);
+			toast.error('Error occurred!');
+		}
+	};
+
 
 	const getUserDetails = async () => {
 		const response = await getUser();
@@ -100,12 +138,45 @@ const Profile = () => {
 								<div className='col-md-6'>
 									<h3 className='text-left '>Watchlist</h3>
 									<hr/>
+									<Table className='font-weight-bolder'>
+										<thead>
+											<th>
+												Symbol
+											</th>
+										</thead>
+										<tbody>
+											{watchList.map((item) => (
+												<tr key={item.id}>{item.symbol}</tr>
+											))}
+										</tbody>
+
+									</Table>
 									<div className='h6 text-left pb-2'>You can add your favourite symbols into watchlist.</div>
 									<div className='font-weight-bold h6 bg-color-3 rounded p-2 cursor-1 w-50'>See More <i className='fa-external-link-square-alt'/> </div>
 								</div>
 								<div className='col-md-6'>
 									<h3 className='text-left '>Price Alerts</h3>
 									<hr/>
+									<Table className='font-weight-bolder'>
+										<thead>
+											<th>
+											Symbol
+											</th>
+											<th>
+												Alert Price
+											</th>
+										</thead>
+										<tbody>
+											{alerts.map((item) => (
+												<tr key={item.id}>
+													<td>{item.symbol}</td>
+													<td>{item.trigger_price}</td>
+												</tr>
+
+											))}
+										</tbody>
+
+									</Table>
 									<div className='h6 text-left pb-2'>You can add price alerts for any symbol</div>
 									<div className='font-weight-bold h6 bg-color-3 rounded p-2 cursor-1 w-50'>See More <i className='fa-external-link-square-alt'/> </div>
 								</div>
